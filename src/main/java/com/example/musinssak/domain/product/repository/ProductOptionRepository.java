0cb583eb542109f7453684f93288f28c4f7f8763
imi21123 <imi21123@naver.com>
2025-09-01 20:57:51 +0900

    feat: 상품 상세 조회 api 구현(repository)

--- src/main/java/com/example/musinssak/domain/product/repository/ProductOptionRepository.java
+++ src/main/java/com/example/musinssak/domain/product/repository/ProductOptionRepository.java
@@ -3,5 +3,9 @@ package com.example.musinssak.domain.product.repository;
 import com.example.musinssak.domain.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+    // 상품 id로 옵션(사이즈/재고/가격) 조회
+    List<ProductOption> findAllByProductIdOrderByIdAsc(Long productId);
 }
